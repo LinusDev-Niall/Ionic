c66e9592652c9ca40eb0e397153b94d4be02044b
Niall Newman <niallnewman@fgx-macbook-niall.local>
2016-10-05 16:36:34 +0100

    Formatting

--- Ionic.php
+++ Ionic.php
@@ -31,153 +31,163 @@ require 'vendor/autoload.php';
 require 'config.php';
 require 'strings.php';
 
-class SmithWatermanGotoh 
+class SmithWatermanGotoh
 {
-	private $gapValue;
-	private $leftSubstitution;
-	
-	/**
-	* Constructs a new Smith Waterman metric.
-	* 
-	* @param gapValue
-	*            a non-positive gap penalty
-	* @param substitution
-	*            a substitution function
-	*/
-	public function __construct($gapValue=-0.5, $leftSubstitution=null) {
-		if($gapValue > 0.0) throw new Exception("gapValue must be <= 0");
-		if (empty($leftSubstitution)) $this->leftSubstitution = new SmithWatermanMatchMismatch(1.0, -2.0);
-		else $this->leftSubstitution = $leftSubstitution;
-		$this->gapValue = $gapValue;
-	}
-	
-	public function compare($left, $right) {
-		if (empty($left) && empty($right)) {
-			return 1.0;
-		}
-		if (empty($left) || empty($right)) {
-			return 0.0;
-		}
-		if (md5($left) == md5($right)) {
-			return 1.0;
-		}
-		
-		$maxDistance = min(mb_strlen($left), mb_strlen($right)) * max($this->leftSubstitution->max(), $this->gapValue);
-		return $this->analyse($left, $right) / $maxDistance;
-	}
-	
-	private function analyse($left, $right) {
-		$deletion = [];
-		$insertion = [];
-		$rightLength = mb_strlen($right);
-		$max = $deletion[0] = max(0, $this->gapValue, $this->leftSubstitution->compare($left, 0, $right, 0));
-		
-		for ($j = 1; $j < $rightLength; $j++) {
-			$deletion[$j] = max(0, $deletion[$j - 1] + $this->gapValue, $this->leftSubstitution->compare($left, 0, $right, $j));
-			$max = max($max, $deletion[$j]);
-		}
-		
-		for ($i = 1; $i < mb_strlen($left); $i++) {
-			$insertion[0] = max(0, $deletion[0] + $this->gapValue, $this->leftSubstitution->compare($left, $i, $right, 0));
-			$max = max($max, $insertion[0]);
-			
-			for ($j = 1; $j < $rightLength; $j++) {
-				$insertion[$j] = max(0, $deletion[$j] + $this->gapValue, $insertion[$j - 1] + $this->gapValue,
-								        $deletion[$j - 1] + $this->leftSubstitution->compare($left, $i, $right, $j));
-				$max = max($max, $insertion[$j]);
-			}
-			
-			for ($j = 0; $j < $rightLength; $j++) {
-				$deletion[$j] = $insertion[$j];
-			}
-		}
-		return $max;
-	}
+    private $gapValue;
+    private $leftSubstitution;
+
+    /**
+     * Constructs a new Smith Waterman metric.
+     *
+     * @param gapValue
+     *            a non-positive gap penalty
+     * @param substitution
+     *            a substitution function
+     */
+    public function __construct($gapValue = -0.5, $leftSubstitution = null)
+    {
+        if ($gapValue > 0.0) throw new Exception("gapValue must be <= 0");
+        $this->leftSubstitution = $leftSubstitution;
+        if (empty($leftSubstitution)) $this->leftSubstitution = new SmithWatermanMatchMismatch(1.0, -2.0);
+        $this->gapValue = $gapValue;
+    }
+
+    public function compare($left, $right)
+    {
+        if (empty($left) && empty($right)) {
+            return 1.0;
+        }
+        if (empty($left) || empty($right)) {
+            return 0.0;
+        }
+        if (md5($left) == md5($right)) {
+            return 1.0;
+        }
+
+        $maxDistance = min(mb_strlen($left), mb_strlen($right)) * max($this->leftSubstitution->max(), $this->gapValue);
+        return $this->analyse($left, $right) / $maxDistance;
+    }
+
+    private function analyse($left, $right)
+    {
+        $deletion = [];
+        $insertion = [];
+        $rightLength = mb_strlen($right);
+        $max = $deletion[0] = max(0, $this->gapValue, $this->leftSubstitution->compare($left, 0, $right, 0));
+
+        for ($j = 1; $j < $rightLength; $j++) {
+            $deletion[$j] = max(0, $deletion[$j - 1] + $this->gapValue, $this->leftSubstitution->compare($left, 0, $right, $j));
+            $max = max($max, $deletion[$j]);
+        }
+
+        for ($i = 1; $i < mb_strlen($left); $i++) {
+            $insertion[0] = max(0, $deletion[0] + $this->gapValue, $this->leftSubstitution->compare($left, $i, $right, 0));
+            $max = max($max, $insertion[0]);
+
+            for ($j = 1; $j < $rightLength; $j++) {
+                $insertion[$j] = max(0, $deletion[$j] + $this->gapValue, $insertion[$j - 1] + $this->gapValue,
+                    $deletion[$j - 1] + $this->leftSubstitution->compare($left, $i, $right, $j));
+                $max = max($max, $insertion[$j]);
+            }
+
+            for ($j = 0; $j < $rightLength; $j++) {
+                $deletion[$j] = $insertion[$j];
+            }
+        }
+        return $max;
+    }
 }
 
 class SmithWatermanMatchMismatch
 {
-	private $matchValue;
-	private $mismatchValue;
-	
-	/**
-	* Constructs a new match-mismatch substitution function. When two
-	* characters are equal a score of <code>matchValue</code> is assigned. In
-	* case of a mismatch a score of <code>mismatchValue</code>. The
-	* <code>matchValue</code> must be strictly greater then
-	* <code>mismatchValue</code>
-	* 
-	* @param matchValue
-	*            value when characters are equal
-	* @param mismatchValue
-	*            value when characters are not equal
-	*/
-	public function __construct($matchValue, $mismatchValue) {
-		if($matchValue <= $mismatchValue) throw new Exception("mismatchValue must be > matchValue");
-		
-		$this->matchValue = $matchValue;
-		$this->mismatchValue = $mismatchValue;
-	}
-	
-	public function compare($left, $leftIndex, $right, $rightIndex) {
-		return ($left[$leftIndex] === $right[$rightIndex] ? $this->matchValue
-				                : $this->mismatchValue);
-	}
-	
-	public function max() {
-		return $this->matchValue;
-	}
-	
-	public function min() {
-		return $this->mismatchValue;
-	}
+    private $matchValue;
+    private $mismatchValue;
+
+    /**
+     * Constructs a new match-mismatch substitution function. When two
+     * characters are equal a score of <code>matchValue</code> is assigned. In
+     * case of a mismatch a score of <code>mismatchValue</code>. The
+     * <code>matchValue</code> must be strictly greater then
+     * <code>mismatchValue</code>
+     *
+     * @param matchValue
+     *            value when characters are equal
+     * @param mismatchValue
+     *            value when characters are not equal
+     */
+    public function __construct($matchValue, $mismatchValue)
+    {
+        if ($matchValue <= $mismatchValue) throw new Exception("mismatchValue must be > matchValue");
+
+        $this->matchValue = $matchValue;
+        $this->mismatchValue = $mismatchValue;
+    }
+
+    public function compare($left, $leftIndex, $right, $rightIndex)
+    {
+        return ($left[$leftIndex] === $right[$rightIndex] ? $this->matchValue
+            : $this->mismatchValue);
+    }
+
+    public function max()
+    {
+        return $this->matchValue;
+    }
+
+    public function min()
+    {
+        return $this->mismatchValue;
+    }
 }
 
 //JSON return functions
 
-function statusSuccess(string $message) {
-	return sprintf("{status: \"success\", message: \"%s\"}", $message);
+function statusSuccess(string $message)
+{
+    return sprintf("{status: \"success\", message: \"%s\"}", $message);
 }
 
-function statusSuccessInsert(string $message, string $docId) {
-	return sprintf("{status: \"success\", message: \"%s\", docId: \"%s\"}", $message, $docId);
+function statusSuccessInsert(string $message, string $docId)
+{
+    return sprintf("{status: \"success\", message: \"%s\", docId: \"%s\"}", $message, $docId);
 }
 
-function statusFail(string $message) {
-	return sprintf("{status: \"fail\", message: \"%s\"}", $message);
+function statusFail(string $message)
+{
+    return sprintf("{status: \"fail\", message: \"%s\"}", $message);
 }
 
-function statusTimeout(string $message) {
-	return sprintf("{status: \"timeout\", message: \"%s\"}", $message);
+function statusTimeout(string $message)
+{
+    return sprintf("{status: \"timeout\", message: \"%s\"}", $message);
 }
 
 //Establish DB connection
 
 if ($use_ssl) {
-	if (!preg_match("/\?ssl=true/", $mongo_server)) {
-		$mongo_server .= (substr($mongo_server, -1) != '/' ? '/?ssl=true' : '?ssl=true');
-	}
-	$conn = new MongoDB\Client($mongo_server, [], [
-		"local_cert" => $ssl_local_cert,
-		"passphrase" => $ssl_cert_passphrase,
-		"cafile" => $ssl_dir . '/' . $ssl_file,
-		"allow_self_signed" => $ssl_allow_self_signed,
-		"verify_peer" => $ssl_verify_peer,
-		"verify_peer_name" => $ssl_verify_peer_name,
-		"verify_expiry" => $ssl_verify_expiry,
-	]);
-}
-else {
-	$conn = new MongoDB\Client($mongo_server);
+    if (!preg_match("/\?ssl=true/", $mongo_server)) {
+        $mongo_server .= (substr($mongo_server, -1) != '/' ? '/?ssl=true' : '?ssl=true');
+    }
+    $conn = new MongoDB\Client($mongo_server, [], [
+        "local_cert" => $ssl_local_cert,
+        "passphrase" => $ssl_cert_passphrase,
+        "cafile" => $ssl_dir . '/' . $ssl_file,
+        "allow_self_signed" => $ssl_allow_self_signed,
+        "verify_peer" => $ssl_verify_peer,
+        "verify_peer_name" => $ssl_verify_peer_name,
+        "verify_expiry" => $ssl_verify_expiry,
+    ]);
+} else {
+    $conn = new MongoDB\Client($mongo_server);
 }
 
 $found = FALSE;
 
 foreach ($conn->listDatabases() as $db_info) {
-	if ($db_info->getName() == $db_name) {
-		$found = TRUE;
-		break;
-	}
+    if ($db_info->getName() == $db_name) {
+        $found = TRUE;
+        break;
+    }
 }
 if (!$found) die(statusFail($err_db_name_invalid));
 
@@ -185,10 +195,10 @@ $db = $conn->$db_name;
 $found = FALSE;
 
 foreach ($db->listCollections() as $coll_info) {
-	if ($coll_info->getName() == $coll_name) {
-		$found = TRUE;
-		break;
-	}
+    if ($coll_info->getName() == $coll_name) {
+        $found = TRUE;
+        break;
+    }
 }
 if (!$found) die(statusFail($err_coll_name_invalid));
 
@@ -206,28 +216,29 @@ if (isset($_GET['file'])) $filePath = $_GET['file'];
 
 
 if (isset($_GET['maxRecur'])) $maxRecur = $_GET['maxRecur']; else $maxRecur = $ion_match_max_recur;
-if (!is_numeric($maxRecur) || $maxRecur < 5 || $maxRecur > 1000)  {
-	die(statusFail(sprintf($err_inv_arg_mrec, escapeshellarg($maxRecur))));
+if (!is_numeric($maxRecur) || $maxRecur < 5 || $maxRecur > 1000) {
+    die(statusFail(sprintf($err_inv_arg_mrec, escapeshellarg($maxRecur))));
 }
 
 //Set up the $left and $file variables
 
 if ($mode == 'string') {
-	if (isset($string)) $string = gzinflate(base64_decode($string)); else die(statusFail($err_inv_arg_str));
-	if (strlen($string) < $ion_bootstrap_min_length || strlen($string) > $ion_bootstrap_max_length) {
-		die(statusFail(sprintf($err_inv_arg_str, strlen($string))));
-	}
-	$left = $string;
-}
-else {
-	if (isset($filePath) && (strlen($filePath) < $sec_fp_min_length || strlen($filePath) > $sec_fp_max_length)) {
-		die(statusFail($err_inv_arg_fp));
-	}
-	$file = file($filePath) or die(statusFail(sprintf($err_file_no_acc, escapeshellarg($filePath))));
-	
-	if (!isset($file[1])) die(statusFail($err_not_ion_file));
-	$left = $file[1];
-	if (strlen($left) > $ion_bootstrap_max_length) statusFail($err_not_ion_file);
+    if (isset($string)) $string = gzinflate(base64_decode($string)); else die(statusFail($err_inv_arg_str));
+    if (strlen($string) < $ion_bootstrap_min_length || strlen($string) > $ion_bootstrap_max_length) {
+        die(statusFail(sprintf($err_inv_arg_str, strlen($string))));
+    }
+    $left = $string;
+} else {
+    if (!isset($filePath) ||
+        (isset($filePath) && ((strlen($filePath) < $sec_fp_min_length || strlen($filePath) > $sec_fp_max_length)))
+    ) {
+        die(statusFail($err_inv_arg_fp));
+    }
+    $file = file($filePath) or die(statusFail(sprintf($err_file_no_acc, escapeshellarg($filePath))));
+
+    if (!isset($file[1])) die(statusFail($err_not_ion_file));
+    $left = $file[1];
+    if (strlen($left) > $ion_bootstrap_max_length) statusFail($err_not_ion_file);
 }
 
 //Analyse the bootsrapper
@@ -235,45 +246,42 @@ else {
 $search = array('md5' => md5($left));
 
 if ($coll->count($search) > 0) {
-	$document = $coll->findOne($search);
-}
-else {
-	$docs = $coll->find();
-	$counter = 0;
-	foreach ($docs as $item) {
-		if ($counter >= $maxRecur) die(statusTimeout($err_match_timeout));
-		
-		$metric = new SmithWatermanGotoh();
-		$sim = $metric->compare($left, base64_decode($item['text']));
-		$counter++;
-		
-		if ($sim > $ion_sim_threshold) {
-			$result = $coll->insertOne(array('md5' => md5($left), 'text' => base64_encode($left), 'sim' => $sim, 'created' => time()));
-			$newDocId = $result->getInsertedId();
-			break;
-		}
-		else {
-			die(statusFail($msg_no_template));
-		}
-	}
+    $document = $coll->findOne($search);
+} else {
+    $docs = $coll->find();
+    $counter = 0;
+    foreach ($docs as $item) {
+        if ($counter >= $maxRecur) die(statusTimeout($err_match_timeout));
+
+        $metric = new SmithWatermanGotoh();
+        $sim = $metric->compare($left, base64_decode($item['text']));
+        $counter++;
+
+        if ($sim > $ion_sim_threshold) {
+            $result = $coll->insertOne(
+                array('md5' => md5($left), 'text' => base64_encode($left), 'sim' => $sim, 'created' => time()));
+            $newDocId = $result->getInsertedId();
+            break;
+        } else {
+            die(statusFail($msg_no_template));
+        }
+    }
 }
 
 //Analyse the base64 encoded block
 
 if ($mode == 'file' && isset($file[2])) {
-	array_splice($file, 0, 2);
-	foreach ($file as $line) {
-		if (strlen($line) < 4) continue;
-		
-		if (preg_match("/^[a-zA-Z0-9+\/]+={0,2}$/", $line) != 1) {
-			die(statusFail($msg_regex_bb_miss . $line));
-		}
-	}
-	$data = isset($newDocId) ? statusSuccessInsert($msg_valid_ioncube, $new_doc_id) : statusSuccess($msg_valid_ioncube);
-}
-else {
-	$data = isset($newDocId) ? statusSuccessInsert($msg_abrupt_f_end, $new_doc_id) : statusSuccess($msg_abrupt_f_end);
+    array_splice($file, 0, 2);
+    foreach ($file as $line) {
+        if (strlen($line) < 4) continue;
+
+        if (preg_match("/^[a-zA-Z0-9+\/]+={0,2}$/", $line) != 1) {
+            die(statusFail($msg_regex_bb_miss . $line));
+        }
+    }
+    $data = isset($newDocId) ? statusSuccessInsert($msg_valid_ioncube, $newDocId) : statusSuccess($msg_valid_ioncube);
+} else {
+    $data = isset($newDocId) ? statusSuccessInsert($msg_abrupt_f_end, $newDocId) : statusSuccess($msg_abrupt_f_end);
 }
 die($data);
 
-?>
